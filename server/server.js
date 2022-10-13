6417ee554e7c870e7a756f5c9b04658bcb643560
Jenna <jennashamza@gmail.com>
2022-10-13 08:16:48 -0700

    more heroku bs

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ const app = express();
 
 const apiRouter = require('./routes/api');
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 //handle parsing request body
 app.use(express.json());
